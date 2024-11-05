bdffffd61cabecdde4304d61c47e6d7417ae7aba
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2024-11-05 21:58:16 +0530

    make correction of userslice

--- src/Redux/UserSlice/index.ts
+++ src/Redux/UserSlice/index.ts
@@ -1,10 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface UserAuth {
+  isLogin: boolean;
+  values: [];
+}
+
+const initialState: UserAuth = {
+  isLogin: false,
+  values: [],
+};
 
 const UserSlice = createSlice({
   name: "UserSlice",
-  initialState: {},
+  initialState,
   reducers: {
-    setuserauth: (state, action) => {},
+    setuserauth: (state, action: PayloadAction<any>) => {
+      state.values = action.payload;
+    },
   },
 });
 export const { setuserauth } = UserSlice.actions;
