7546a6e5bea16eb49675b63e7195f8a5a943d5b2
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2024-11-12 17:54:20 +0530

    udpated code

--- src/Services/apiservice/apiInstance.ts
+++ src/Services/apiservice/apiInstance.ts
@@ -4,4 +4,21 @@ const apiInstance = axios.create({
   baseURL: "http://localhost:8000/api",
 });
 
+apiInstance.interceptors.request.use(
+  (config) => {
+    const getToken: string | null = localStorage.getItem("token");
+    const token = getToken ? JSON.parse(getToken) : null;
+
+    // Ensure headers exist before assigning Authorization
+    if (token && config.headers) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error: any) => {
+    return Promise.reject(error.message);
+  }
+);
+
 export default apiInstance;
